c7730e42fb8ee98bdefe7024ee97aed91828d088
bs.wiwat@gmail.com <>
2024-04-22 16:01:02 +0700

    add(Response)addNew-Response

---
+++ Usermanagement.API/CodePluse.API/DTO/Reponse/ResponseDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static CodePluse.API.DTO.Reponse.ResponseDTO;
+
+namespace CodePluse.API.DTO.Reponse
+{
+	public class ResponseDTO
+	{
+        public class Status
+        {
+            public string Code { get; set; }
+
+            public string Description { get; set; }
+        }
+
+        public class Data
+        {
+            public bool Result { get; set; }
+
+            public string Message { get; set; }
+        }
+
+        public Status status { get; set; }
+
+        public Data data { get; set; }
+
+        //public List<RoleDTO> Role { get; set; }
+
+        //public List<PermissionDTO> Permission { get; set; }
+    }
+}
+
